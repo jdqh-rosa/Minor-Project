1f364e3ac777603f77ebde00e3b16ff71151d0de
Jdqh <58776824+jdqh-rosa@users.noreply.github.com>
2025-02-19 17:31:48 +0100

    Initial Commit/2

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    GameInput gameInput = new GameInput();
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
